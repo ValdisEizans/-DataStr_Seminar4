7da5e2ab9a8a40ec407dfb8b8e06f8beb0610d4f
ValdisEizans <valdis.eizans@gmail.com>
2026-05-14 09:28:23 +0300

    Edit add function added

--- src/datastr/MyBST.java
+++ src/datastr/MyBST.java
@@ -31,28 +31,43 @@ public class MyBST<Ttype>{
 		if(isFull()) {
 			throw new Exception("Koks ir pilns, nevar pievienot elementu!");
 		}
+		//Ja koks tukss, ieliek pirmo ka root
+		if(isEmpty()) {
+			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+			rootNode = newNode;
+		}
+		else {
 		addHelper(rootNode, element);
+		}
 		howManyElements++;
-		
 	}
 	
 	private void addHelper(MyNode<Ttype> nodeTemp, Ttype element) {
 		if(nodeTemp != null) {
-			if(((Comparable)element).compareTo(nodeTemp.getElement()) > 0 ) {
+			if(((Comparable)element).compareTo(nodeTemp.getElement()) > 0 ) {//ja lielaks, iet pa labi
+				if(nodeTemp.getRightChildNode() == null) {//laba puse neka nav
+					MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+					newNode.setParrentNode(nodeTemp);
+					nodeTemp.setRightChildNode(newNode);
+				}
+				else {//laba puse jau ir bloks, tapec rekursivi izsauc so pasu funkciju
 				addHelper(nodeTemp.getRightChildNode(), element);
-				
+				}
 			}
-			else {
-				addHelper(nodeTemp.getLeftChildNode(), element);
-
+			else {//ja mazaks, iet pa kreisi
+				if(nodeTemp.getLeftChildNode() == null) {//kreisa puse neka nav
+					MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+					newNode.setParrentNode(nodeTemp);
+					nodeTemp.setLeftChildNode(newNode);
+				}
+				else {//kreisa puse jau ir bloks, tapec rekursivi izsauc so pasu funkciju
+					addHelper(nodeTemp.getLeftChildNode(), element);
+				}
 			}
-			
-			
-			
-			
-			
-			
 		}
+		
+		
+		
 	}
 	
 	
