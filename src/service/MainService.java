88c0e1f7afd20cbf025d3885845b935d7f357e0a
ValdisEizans <valdis.eizans@gmail.com>
2026-05-14 09:42:40 +0300

    Edit service function  added

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,10 +1,24 @@
 package service;
 
+import datastr.MyBST;
+
 public class MainService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		MyBST<Integer> bstKoks = new MyBST<Integer>();
+		try {
+			bstKoks.add(10);
+			bstKoks.add(6);
+			bstKoks.add(23);
+			bstKoks.add(8);
+			bstKoks.add(7);
+			System.out.println("----------------------------------------------------------------------------");
+			bstKoks.print();
+			
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
